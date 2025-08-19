730dc11d50e742513df5a11eb4af031139a85d95
zhhr1991 <aaliyan.zh@gmail.com>
2025-08-19 16:58:56 +0330

    fixed error

--- backend/config/upstashRateLim.js
+++ backend/config/upstashRateLim.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const rateLimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100, "60 s"), // 100 requests per 60 seconds
+    limiter: Ratelimit.slidingWindow(10, "60 s"), // 100 requests per 60 seconds
     analytics: true,
 });
 
